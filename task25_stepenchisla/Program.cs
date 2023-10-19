faa4bf7d628835912ff657601eea198de2c7c2ac
masha <hanchuk91@gmail.com>
2023-10-20 00:44:44 +0300

    seminar4dz

---
+++ task25_stepenchisla/Program.cs
@@ -0,0 +1,17 @@
+﻿//  Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+void Exponentation (int a, int b)
+{
+    int result = 1;
+    for (int i = 0; i < b; i++)
+    {
+        result *=a;
+    }
+    Console.WriteLine(result);
+}
+Console.Write("Введите число А: ");
+int A = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число B: ");
+int B = Convert.ToInt32(Console.ReadLine());
+Exponentation(A, B);
---
+++ task27_summachisel/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+int SumDigit (int a)
+{
+    int sum=0;
+    for (int i = a; i > 0; i/=10)
+    {
+        sum=sum%10+i;
+    }
+    return sum;
+}
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(SumDigit(number));
